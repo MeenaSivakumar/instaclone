c5e775085ec778423370b14ac3d6e057f069a18a
MeenaSivakumar <git config --global user.email>
2024-10-26 16:46:37 +0530

    ten

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -56,16 +58,26 @@ class _SearchScreenState extends State<SearchScreen> {
                 return ListView.builder(
                     itemCount: (snapshot.data! as dynamic).docs.length,
                     itemBuilder: (context, index) {
-                      return ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            ((snapshot.data! as dynamic).docs[index]
-                                ['photourl']),
+                      return InkWell(
+                        onTap: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => ProfileScreen(
+                              uid: (snapshot.data! as dynamic).docs[index]['uid'],
+                                  
+                            ),
                           ),
-                          radius: 16,
                         ),
-                        title: Text(
-                          (snapshot.data! as dynamic).docs[index]['username'],
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              ((snapshot.data! as dynamic).docs[index]
+                                  ['photourl']),
+                            ),
+                            radius: 16,
+                          ),
+                          title: Text(
+                            (snapshot.data! as dynamic).docs[index]['username'],
+                          ),
                         ),
                       );
                     });
@@ -78,7 +90,9 @@ class _SearchScreenState extends State<SearchScreen> {
                     child: CircularProgressIndicator(),
                   );
                 }
-                return MasonryGridView.count(crossAxisCount: 3,itemCount: (snapshot.data! as dynamic).docs.length,
+                return MasonryGridView.count(
+                  crossAxisCount: 3,
+                  itemCount: (snapshot.data! as dynamic).docs.length,
                   itemBuilder: (context, index) => Image.network(
                     (snapshot.data! as dynamic).docs[index]['postUrl'],
                     fit: BoxFit.cover,
